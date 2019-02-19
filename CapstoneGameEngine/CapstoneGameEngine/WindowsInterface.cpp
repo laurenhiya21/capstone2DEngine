65d614f45ec43fc9e356029ae68d03d9967932ea
laurenhiya21 <44984339+laurenhiya21@users.noreply.github.com>
2019-02-18 20:31:28 -0800

    Make a window
    Makes a simple window that doesn't do anything (exit with escape key)
    Includes glad and glfw (both open source under cc0)

--- CapstoneGameEngine/CapstoneGameEngine/WindowsInterface.cpp
+++ CapstoneGameEngine/CapstoneGameEngine/WindowsInterface.cpp
@@ -9,25 +9,25 @@ void WindowsInterface::start()
 	// MIGHT NEED TO MAKE A WINMAIN FUNCTION TO CALL THIS STUFF?
 
 	// register the window class
-	const wchar_t CLASS_NAME[] = "TestWindow";
+	//const wchar_t CLASS_NAME[] = "TestWindow";
 
 	// create a window
-	HWND newWindow = CreateWindowEx(
-		0,
-		CLASS_NAME,
-		"Test Window",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-		);
+	//HWND newWindow = CreateWindowEx(
+		//0,
+		//CLASS_NAME,
+		//"Test Window",
+		//WS_OVERLAPPEDWINDOW,
+		//CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		//NULL,
+		//NULL,
+		//hInstance,
+		//NULL
+		//);
 
-	if (newWindow == NULL)
-	{
-		std::cout << "no window what the heck" << std::endl;
-	}
+	//if (newWindow == NULL)
+	//{
+	//	std::cout << "no window what the heck" << std::endl;
+	//}
 }
 
 // continue running the WindowsINterface system

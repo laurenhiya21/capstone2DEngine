a44849a194cd9ba221ee1cf2b9b507379fffc536
laurenhiya21 <44984339+laurenhiya21@users.noreply.github.com>
2019-06-19 13:33:12 -0700

    Start of keyboard input implementation
    Just starting getting input for keyboard. Made basic container for the keys used, their state (up, pressed, down, or released) and (later) the action associated with that key

--- CapstoneGameEngine/CapstoneGameEngine/Input.cpp
+++ CapstoneGameEngine/CapstoneGameEngine/Input.cpp
@@ -1,5 +1,5 @@
 #include "Input.h"
-
+#include <iostream> //TEMP OUTPUT
 
 // constructor
 Input::Input()
@@ -9,11 +9,33 @@ Input::Input()
 // run/update the input system
 void Input::run()
 {
+	//std::cout << "Space" << glfwGetKey(sysHeadHancho.mainWindow.windowPtr, GLFW_KEY_SPACE) << std::endl;
+
 	// mostly from old stuff cause simple and it's TEMPPPPPP----------------------------------------------
 	if (glfwGetKey(sysHeadHancho.mainWindow.windowPtr, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		sysHeadHancho.exit();
 		//glfwSetWindowShouldClose(sysHeadHancho.mainWindow.windowPtr, GL_TRUE);
+	}
+
+	if (glfwGetKey(sysHeadHancho.mainWindow.windowPtr, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		std::cout << "W Pressed!" << std::endl;
+	}
+
+	if (glfwGetKey(sysHeadHancho.mainWindow.windowPtr, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		std::cout << "S Pressed!" << std::endl;
+	}
+
+	if (glfwGetKey(sysHeadHancho.mainWindow.windowPtr, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		std::cout << "A Pressed!" << std::endl;
+	}
+
+	if (glfwGetKey(sysHeadHancho.mainWindow.windowPtr, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		std::cout << "D Pressed!" << std::endl;
 	}
 }
 
--- CapstoneGameEngine/CapstoneGameEngine/Input.h
+++ CapstoneGameEngine/CapstoneGameEngine/Input.h
@@ -1,17 +1,36 @@
 #pragma once
 
 #include "HeadHancho.h"
+#include <vector> // used to contain keys + their states/actions
+
+// states that any key on the keyboard can be in
+enum KeyState
+{
+	UP,
+	PRESSED,
+	DOWN,
+	RELEASED
+};
 
 // Handles all the inputs to the game
 class Input: public System
 {
 	private:
-		// probably need something for all keyboard keys
+		// container for state of keyboard key 
+		// and action associated with it
+		class InputKey
+		{
+			KeyState state; // state of the key (up, pressed, down, released)
+			int key; // int that corrosponds to the GLFW keyboard define
+			//action here
+		};
+		
+		std::vector<KeyState> keyArray; // list of the keys used
+
 		// someting for if keys dealt with
 		// mouse postion?
 		// similar to keyboard keys but mouse keys?
 		// controlelr stuff?
-		// time variables?
 
 	public:
 
